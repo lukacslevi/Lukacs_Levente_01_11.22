47602440fb09703056773b565408305852c03616
lukacslevi <lukacs.levente@students.jedlik.eu>
2022-11-24 12:38:45 +0100

    menü

--- functions.py
+++ functions.py
@@ -2,3 +2,12 @@ from data import vonatok, ido
 from os import system
 fajlnev='vonatok.csv'
 
+def menu():
+    system('cls')
+    print('------------MENÜ------------')
+    print('0 - Kilépés')
+    print('1 - Személyvonatok')
+    print('2 - Szerelvények kilistázása')
+    print('3 - Új szerelvény felvétele')
+    print('4 - Szerelvény törlése')
+    return input('Választás: ')
